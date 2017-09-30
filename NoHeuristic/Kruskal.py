af463580b660181660be492cbca5ad01656e2074
Mohammed Najib Haouas <haouas.najib@hotmail.com>
2017-09-30 00:21:11 -0400

    Initial

--- NoHeuristic/Kruskal.py
+++ NoHeuristic/Kruskal.py
@@ -40,25 +40,4 @@ class Kruskal(object):
         self.g = g
 
     def getMSTCost(self, sol, source):
-        mst_cost = 0
-        self.uf.reset()
-
-        all_sorted_edges = self.g.get_sorted_edges()
-        useful_sorted_edges = []
-
-        if not sol.visited:
-            useful_sorted_edges = all_sorted_edges
-        else:
-            for i_edge in all_sorted_edges:
-                if i_edge.source in sol.not_visited and i_edge.destination in sol.not_visited:
-                    useful_sorted_edges.append(i_edge)
-                elif (i_edge.source == sol.visited[len(sol.visited)-1] and i_edge.destination in sol.not_visited) or \
-                        (i_edge.destination == sol.visited[len(sol.visited)-1] and i_edge.source in sol.not_visited):
-                    useful_sorted_edges.append(i_edge)
-
-        for e_edge in useful_sorted_edges:
-            if not self.uf.makes_cycle(e_edge):
-                self.uf.add(e_edge)
-                mst_cost += e_edge.cost
-
-        return mst_cost
+        raise NotImplementedError()
